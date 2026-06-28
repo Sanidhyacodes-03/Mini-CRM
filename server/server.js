63a691f369cff26ae4bb97d0e82a9ed6a92d8162
Sanidhya Singh Sisodiya <sanidhya.s.sisodia033@gmail.com>
2026-06-28 22:55:48 +0530

    Add production deployment support (Render + Vercel)

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : ['http://localhost:5173', 'http://localhost:3000'],
   credentials: true,
 }));
 app.use(express.json());
